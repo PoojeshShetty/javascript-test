d75edd8aac07e1d66e9fe50cd33123b03eef9b2a
PoojeshShetty <shettypoojesh@live.com>
2022-02-27 23:25:42 +0530

    update module

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import '@babel/polyfill';
 import http from 'http';
 
 function requestHandler(req, res) {
-  res.writeHead(200, { 'content-Type': 'text/plain' })
+  res.writeHead(200, { 'content-Type': 'text/plain' });
   res.end('hello, world this is pj');
 };
 const server = http.createServer(requestHandler)
